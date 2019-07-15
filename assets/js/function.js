d7fa1c6ad3dd891b930861c0ddc8b2e34e66eb0d
Szabó Balázs <szabo.balazs2017@gmail.com>
2019-07-15 20:39:13 +0300

    Markup, style and some js for about section

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,63 @@
+---
+---
+
 $(document).ready(function(){
 
-  
+  showCopyright();
+  setAboutCarouselBackground();
+  aboutCarousel();
 
 });
+
+function showCopyright()
+{
+  var date = new Date();
+  $(".copyright").text("Copyright " + date.getFullYear() + " | {{ site.data.basic.developer }}");
+}
+
+function setAboutCarouselBackground()
+{
+  {% for item in site.data.about-carousel %}
+
+  $("#about .about-carousel .slide:nth-child({{ forloop.index }})").css("background-image", "url('assets/img/about-carousel/{{ item.url }}')");
+
+  {% endfor %}
+}
+
+function aboutCarousel()
+{
+  var currSlideNo = 0;
+  var slideSize = $("#about .carousel-images .slide").length;
+
+  $("#about .controlls .move-left").click(function(){
+    if (currSlideNo > 0)
+    {
+      showSlide(currSlideNo - 1, currSlideNo)
+      currSlideNo--;
+    }
+    else
+    {
+      showSlide(slideSize - 1, currSlideNo)
+      currSlideNo = slideSize - 1;
+    }
+  });
+
+  $("#about .controlls .move-right").click(function(){
+    if (currSlideNo < slideSize - 1)
+    {
+      showSlide(currSlideNo + 1, currSlideNo);
+      currSlideNo++;
+    }
+    else
+    {
+      showSlide(0, currSlideNo);
+      currSlideNo = 0;
+    }
+  });
+}
+
+function showSlide(showIndex, hideIndex)
+{
+  $("#about .carousel-images .slide").eq(showIndex).css("opacity", "1");
+  $("#about .carousel-images .slide").eq(hideIndex).css("opacity", "0");
+}
