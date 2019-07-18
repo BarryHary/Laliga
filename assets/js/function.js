3f1c86961e345311e5dde7793763ecd9aada807a
Szabó Balázs <szabo.balazs2017@gmail.com>
2019-07-18 23:17:37 +0300

    Add license, Smooth scroll with jQuery (browser independent function), About carousel autoplay

--- assets/js/function.js
+++ assets/js/function.js
@@ -3,11 +3,32 @@
 
 $(document).ready(function(){
 
+  smoothScroll(800, -80); // speed, offset
+
   showCopyright();
   setAboutCarouselBackground();
-  aboutCarousel();
+  aboutCarousel(5000); // autoplaySpeed
 
 });
+
+function smoothScroll(speed, offset)
+{
+  $("a").click(function(){
+
+    if (this.hash != "")
+    {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top + offset
+      }, speed, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+}
 
 function showCopyright()
 {
@@ -24,35 +45,29 @@ function setAboutCarouselBackground()
   {% endfor %}
 }
 
-function aboutCarousel()
+function aboutCarousel(autoplaySpeed)
 {
-  var currSlideNo = 0;
-  var slideSize = $("#about .carousel-images .slide").length;
+  var currSlide = {currSlideNo: 0};
+
+  // AUTOPLAY - speed
+  var autoplay = setInterval(function(){
+    moveSlideRight(currSlide)
+  }, autoplaySpeed);
 
   $("#about .controlls .move-left").click(function(){
-    if (currSlideNo > 0)
-    {
-      showSlide(currSlideNo - 1, currSlideNo)
-      currSlideNo--;
-    }
-    else
-    {
-      showSlide(slideSize - 1, currSlideNo)
-      currSlideNo = slideSize - 1;
-    }
+    moveSlideLeft(currSlide);
+    clearInterval(autoplay);
+    autoplay = setInterval(function(){
+      moveSlideRight(currSlide)
+    }, autoplaySpeed);
   });
 
   $("#about .controlls .move-right").click(function(){
-    if (currSlideNo < slideSize - 1)
-    {
-      showSlide(currSlideNo + 1, currSlideNo);
-      currSlideNo++;
-    }
-    else
-    {
-      showSlide(0, currSlideNo);
-      currSlideNo = 0;
-    }
+    moveSlideRight(currSlide);
+    clearInterval(autoplay);
+    autoplay = setInterval(function(){
+      moveSlideRight(currSlide)
+    }, autoplaySpeed);
   });
 }
 
@@ -60,4 +75,36 @@ function showSlide(showIndex, hideIndex)
 {
   $("#about .carousel-images .slide").eq(showIndex).css("opacity", "1");
   $("#about .carousel-images .slide").eq(hideIndex).css("opacity", "0");
+}
+
+function moveSlideLeft(currSlide)
+{
+  const slideSize = $("#about .carousel-images .slide").length;
+
+  if (currSlide.currSlideNo > 0)
+  {
+    showSlide(currSlide.currSlideNo - 1, currSlide.currSlideNo)
+    currSlide.currSlideNo--;
+  }
+  else
+  {
+    showSlide(slideSize - 1, currSlide.currSlideNo)
+    currSlide.currSlideNo = slideSize - 1;
+  }
+}
+
+function moveSlideRight(currSlide)
+{
+  const slideSize = $("#about .carousel-images .slide").length;
+
+  if (currSlide.currSlideNo < slideSize - 1)
+  {
+    showSlide(currSlide.currSlideNo + 1, currSlide.currSlideNo);
+    currSlide.currSlideNo++;
+  }
+  else
+  {
+    showSlide(0, currSlide.currSlideNo);
+    currSlide.currSlideNo = 0;
+  }
 }
